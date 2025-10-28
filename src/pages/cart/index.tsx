96762299c8aebda6b838e94b0665d30885823837
marceloAugustoSG <marcelo_augusto.soares@hotmail.com>
2025-10-28 09:29:32 -0300

    adicionando mock fake-api, ajustes no carrinho

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,5 +1,31 @@
 export default function Cart() {
     return (
-        <h1>Carrinho de compras</h1>
+        <div className="w-full max-w-7xl mx-auto">
+
+            <h1 className="font-medium text-2xl text-center my-4">Meu carrinho</h1>
+
+            <section className="flex items-center justify-between border-b-2 border-gray-300">
+                <img
+                    className="w-28"
+                    src="https://images.tcdn.com.br/img/img_prod/993134/fone_de_ouvido_sem_fio_branco_bluetooth_v5_4_a_gold_fn_ba10_1584_1_e24411cb33aab866507085080e04ff11.jpg"
+                    alt="logo do produto"
+                />
+
+                <strong>Preço: R$1.000</strong>
+                <div className="flex items-center justify-center gap-3">
+                    <button className="bg-slate-600 px-2 rounded text-white font medium flex items-center justify-content">
+                        -
+                    </button>
+                    2
+                    <button className="bg-slate-600 px-2 rounded text-white font medium flex items-center justify-content">
+                        -
+                    </button>
+
+                </div>
+                <strong className="float-right">SubTotal: R$1.000</strong>
+            </section>
+            <p className="font-bold mt-4">Total:R$:1.000</p>
+
+        </div>
     )
 }
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,26 +1,52 @@
 import { BsCartPlus } from "react-icons/bs"
+import { useEffect, useState } from "react"
+import { api } from "../../services/api"
+
+interface ProductProps {
+    id: number;
+    title: string;
+    description: string;
+    price: number;
+    cover: string;
+
+}
 export default function Home() {
+
+    const [products, setProducts] = useState<ProductProps[]>([])
+
+    useEffect(() => {
+        async function getProducts() {
+            const response = await api.get("/products")
+            setProducts(response.data)
+        }
+        getProducts()
+    }, [])
+
     return (
         <div>
             <main className="w-full max-w-7xl px-4 mx-auto">
                 <h1 className="font-bold text-2xl mb-4 mt-10 text-center">Produtos em alta</h1>
                 <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-5"  >
+                    {products.map((product) => (
+                        <section className="w-full">
+                            <img
+                                className="w-full rounded-lg mb-h-70 mb-2"
+                                src={product.cover} alt="logo do produto" />
+                            <p className="font-medium mt-2 mb-2">{product.title}</p>
+                            <div className="flex gap-3 items-center">
+                                <strong className="text-zinc-700/90">{product.price.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</strong>
+                                <button className="bg-zinc-900 p-1 rounde">
+                                    <BsCartPlus size={22} color="#fff" />
+                                </button>
+                            </div>
 
-                    <section className="w-full">
-                        <img
-                            className="w-full rounded-lg mb-h-70 mb-2"
-                            src="https://images.tcdn.com.br/img/img_prod/993134/fone_de_ouvido_sem_fio_branco_bluetooth_v5_4_a_gold_fn_ba10_1584_1_e24411cb33aab866507085080e04ff11.jpg" alt="logo do produto" />
-                        <p className="font-medium mt-2 mb-2">Fone de ouvido sem fio</p>
-                        <div className="flex gap-3 items-center">
-                            <strong className="text-zinc-700/90">R$ 100,00</strong>
-                            <button className="bg-zinc-900 p-1 rounde">
-                                <BsCartPlus size={22} color="#fff" />
-                            </button>
-                        </div>
+                        </section>
 
-                    </section>
 
-                   
+
+
+                    ))}
+
                 </div>
             </main>
         </div>
